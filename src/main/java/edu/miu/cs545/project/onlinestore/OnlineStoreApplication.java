bc5f6e4cd296bbaddf008eb2b915381409ba4f29
simbarashenicholaschitovhoro <snchitovhoro@gmail.com>
2021-08-09 16:28:09 -0500

    Initial commit

---
+++ src/main/java/edu/miu/cs545/project/onlinestore/OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545.project.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/cs545/project/onlinestore/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545.project.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
